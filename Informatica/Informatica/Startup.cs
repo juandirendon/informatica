e5dd1e5295d7ba5fcaa24578c04ccc4db594b674
PaulaDuque <pauladuque13@hotmail.com>
2017-09-13 06:52:04 -0500

    Creación del proyecto.

---
+++ Informatica/Informatica/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Informatica.Startup))]
+namespace Informatica
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
